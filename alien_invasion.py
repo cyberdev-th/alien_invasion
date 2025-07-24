d9205879f0be02c3ea7bdb50ea4aa466f4082af5
Thiago Fernandes <devcyber.thiago@gmail.com>
2025-07-24 10:28:57 -0300

    Criação da classe principal com método de criação da tela inicial do jogo

---
+++ alien_invasion.py
@@ -0,0 +1,33 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    """
+    Classe geral para gerenciar ativos e comportamento do jogo.
+    """
+    def __init__(self):
+        """
+        Inicializa o jogo e cria os recursos do jogo.
+        """
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+    
+    def run_game(self):
+        """
+        Inicia o loop principal do jogo.
+        """
+        while True:
+            # Observa os eventos de mouse e teclado.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            # Torna o desenho de tela mais recente visível.
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    # Cria uma instância e roda o jogo.
+    ai = AlienInvasion()
+    ai.run_game()
