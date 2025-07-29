ac0ae6b2a86143236d366a4660b1359cd73f8b8b
Thiago Fernandes <devcyber.thiago@gmail.com>
2025-07-29 10:34:37 -0300

    Jogo agora é visualizado em tela cheia

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,11 +12,13 @@ class AlienInvasion:
         """
         Inicializa o jogo e cria os recursos do jogo.
         """
-        pygame.init() # liga o motor do jogo
-        self.clock = pygame.time.Clock() # cria um relógio para controlar a taxa de frames do jogo
-        self.settings = Settings() # cria uma instância de configurações no jogo
+        pygame.init() 
+        self.clock = pygame.time.Clock() 
+        self.settings = Settings() 
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
