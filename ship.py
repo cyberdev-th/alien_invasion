56a3fce223512bd419b79ebec3654b19a677dfb6
Thiago Fernandes <devcyber.thiago@gmail.com>
2025-07-29 10:07:04 -0300

    Atualização de movimento para esquerda e modelagem de choque com as bordas

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """Inicializa o navio e configura a posição inicial."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # carregando a imagem do navio an pegando seu retângulo.
@@ -15,13 +16,20 @@ class Ship:
         # inicia cada novo barco na parte baixa e ao centro da tela
         self.rect.midbottom = self.screen_rect.midbottom
 
+        self.x = float(self.rect.x)
+
         # flag de movimento
         self.moving_right = False
+        self.moving_left = False
     
     def update(self):
         """Atualiza a posição do navio."""
-        if self.moving_right:
-            self.rect.x += 1    
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed    
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        
+        self.rect.x = self.x
 
     def blitme(self):
         """Desenha o barco em sua localização atual."""
